14c47d64f35503d4f1c9aaf78e9a6ee37893a71a
csteifel <csteifel@gmail.com>
2015-03-23 23:49:01 -0400

    First window!!!  Also its segfaulting whenever I close the window, I think it might be a problem with the build of the wxWidgets library or something and not actually something that I am doing wrong.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <wx/wx.h>
+#include <memory>
+#include <iostream>
 
+
+class windowDeleter {
+	public:
+		void operator()(wxFrame* window){window->Destroy();}
+};
+
+class mainApp : public wxApp {
+	public:
+		mainApp();
+		bool OnInit();
+	private:
+		std::unique_ptr<wxFrame, windowDeleter> mainWindow;
+};
 
 IMPLEMENT_APP(mainApp);
 
+mainApp::mainApp(){
+}
 
-
-class mainApp : public wxApp {
-
-};
+bool mainApp::OnInit(){
+	mainWindow.reset(new wxFrame(NULL, wxID_ANY, "Test"));
+	mainWindow->Show(true);
+	SetTopWindow(mainWindow.get());
+	return true;
+}
